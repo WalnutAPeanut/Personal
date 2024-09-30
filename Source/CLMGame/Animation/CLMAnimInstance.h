06cd89361f1771fc8ab28286d4f0656e981a8418
[Home] <rbals1101@gmail.com>
2024-09-30 10:03:59 +0900

    [프레임 워크]
    테스트 캐릭터, 모션 및 모션 추가

--- Source/CLMGame/Animation/CLMAnimInstance.h
+++ Source/CLMGame/Animation/CLMAnimInstance.h
@@ -13,5 +13,10 @@ UCLASS()
 class CLMGAME_API UCLMAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+	/** 해당 속성값은 Lyra의 AnimBP에서 사용되는 값이므로 정의해주자 */
+	UPROPERTY(BlueprintReadOnly, Category = "Character State Data")
+	float GroundDistance = -1.0f;
 	
 };
